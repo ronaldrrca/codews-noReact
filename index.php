7b29e0a0a7ad25b92f69d4d5ac9363ddeacc49c4
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2022-05-20 11:12:47 -0500

    Creando la estructura básica de carpetas

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/generalStyles.css">
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+    <script src="./js/index.js"></script>
+</body>
+</html>
